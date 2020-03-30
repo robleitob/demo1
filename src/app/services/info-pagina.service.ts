af4f18a0ae0fd670dd09c1362c0fac710bac126c
Roberto Leiva <robleitob@gmail.com>
2020-03-29 23:23:11 -0300

    Aplicacion lista para pre-PROD

--- src/app/services/info-pagina.service.ts
+++ src/app/services/info-pagina.service.ts
@@ -10,18 +10,37 @@ export class InfoPaginaService {
   info: InfoPagina = {};
   cargada = false;
 
-  constructor( private http: HttpClient) {
+  equipo: any [] = [];
 
+  constructor( private http: HttpClient) {
+    
+    this.cargarInfo();
+    this.cargarEquipo();
+
+   }
+
+   private cargarInfo () {
     // console.log('Servicio de infoPagina corriendo');
     // Necesito algo que me permita leer mi archivo JSON
     this.http.get('assets/data/data-pagina.json')
     .subscribe( (resp: InfoPagina) => {
       this.cargada = true;
       this.info = resp;
+      // console.log(resp);
+      // tslint:disable-next-line: no-string-literal
+      // console.log(resp['email']);
+    });
+   }
+
+   private cargarEquipo(){
+    this.http.get('https://portafolio-angular-18aa2.firebaseio.com/equipo.json')
+    .subscribe( (resp: any[]) => {
+      
+      this.equipo = resp;
       console.log(resp);
       // tslint:disable-next-line: no-string-literal
       // console.log(resp['email']);
     });
-
    }
+
 }
